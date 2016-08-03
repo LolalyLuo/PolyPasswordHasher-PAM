6c3c43855e937b2f8e49cd50d5bd2d7f349c14fb
Lolaly <sl5419@nyu.edu>
2016-08-03 16:59:15 -0400

    finished most features of PAM, still need to fix some details

--- PPHpam.c
+++ PPHpam.c
@@ -24,7 +24,9 @@ struct _gfshare_ctx {
 };
 
 void get_secret(pph_context *context);
-void get_share(pph_context *context);
+void get_share_context(pph_context *context);
+void store_secret(pph_context *context);
+void store_share_context(pph_context *context);
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
 	pam_syslog(pamh, LOG_NOTICE, "PPH: Set credenticial successed!\n");
@@ -60,8 +62,11 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	int retry = 0;
 	pph_context *context;
 	
+	pph_account_node *search;
+	pph_account_node *target = NULL;
+	uint8 sharenumber;
 	
-	//return PAM_AUTH_ERR;
+	//return PAM_SUCCESS;
 	//set up syslog
 	pam_syslog(pamh, LOG_INFO, "PPH: pam_sm_authenticate is being called. \n");
 	
@@ -73,22 +78,16 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	}
 	pam_syslog(pamh, LOG_INFO, "PPH: context is loaded. \n");
 	
+
+	context->is_normal_operation = false;
 	//load secret and share context if available. 
 	get_secret(context);
-	get_share(context);
+	get_share_context(context);
 	//if the secret and share are still not available, try to unlock them		
-	if (context->secret == NULL || context->share_context == NULL) {
-	
-	}
-	//after unlock the secret &shares, if it's still not available, inform system
-	//if it is available, set to normal operation and inform the system
-	if(context->secret == NULL || context->share_context == NULL){
-		pam_syslog(pamh, LOG_ERR,"The secret or share context is still not available\n");
-	} else {
+	if (context->secret != NULL && context->share_context != NULL) {
 		context->is_normal_operation = true;
 		pam_syslog(pamh, LOG_ERR,"Both secret and share context are now available\n");
 	}
-
 
 	//authenticate the user with PPH
 	//get username from user
@@ -117,14 +116,34 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	}else {
 		pam_syslog(pamh, LOG_INFO, "PPH: Fail to authenticate the user for other errors \n");
 	}
+	
+
+	//If the account authenticated is a protector account, put it into a file
+	//for bootstrapping purpose
+	if (error == PPH_ERROR_OK && context->is_normal_operation == false){
+		search = context->account_data;
+		while(search!=NULL){
+			// we check lengths first and then compare what's in it. 
+			if(strlen(username) == search->account.username_length && 
+		          !memcmp(search->account.username,username,strlen(username))){
+			target = search;
+			break;
+   			}
+   		search=search->next;
+  		}	
+		sharenumber = target->account.entries->share_number;
+		if (sharenumber != SHIELDED_ACCOUNT && sharenumber != BOOTSTRAP_ACCOUNT){
+			store_account(username, password);
+			unlock_context(context);
+			}
+		}
+	}
 
 	
 	//after authentication, destroy the context we used
 	if (pph_destroy_context(context) != PPH_ERROR_OK){
 		pam_syslog(pamh, LOG_ERR, "PPH: Can't destroy context\n");
-		return PAM_AUTH_ERR;
 	}
-	pam_syslog(pamh, LOG_ERR, "PPH: before return ,the error is %d, context operation is: %d \n", error, 4);
 	
 	//if autheticated successfully and the secret is not available, 
 	//check if the account is a protector account and save shares
@@ -173,11 +192,18 @@ PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const c
 		pam_syslog(pamh, LOG_ERR, "PPH: Can't open context\n");
 		return PAM_AUTHTOK_LOCK_BUSY;
 	}
+	
+	context->is_normal_operation = false;
+	//load secret and share context if available. 
 	get_secret(context);
-	//if the secret is available, inform the system		
-	if (context->secret != NULL) {
-		pam_syslog(pamh, LOG_ERR,"secret is now available, it is %s\n", context->secret);
+	get_share_context(context);
+	//if the secret and share are still not available, try to unlock them		
+	if (context->secret != NULL && context->share_context != NULL) {
+		context->is_normal_operation = true;
+		pam_syslog(pamh, LOG_ERR,"Both secret and share context are now available\n");
 	}
+
+
 	//try to use the user name and password to create a user
 	error = pph_create_account(context, username, strlen(username), password, strlen(password), 0);
 	pam_syslog(pamh, LOG_ERR, "PPH: the return value of pph_create_account is %d\n", error);
@@ -194,12 +220,10 @@ PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const c
 	//up to here we can store/destroy context
 	if (pph_store_context(context, "/home/lolaly/PolyPasswordHasher-PAM/PPHdata") != PPH_ERROR_OK){
 		pam_syslog(pamh, LOG_ERR, "PPH: Can't store context %d \n", error);
-		exit(1);
 	}
 	
 	if (pph_destroy_context(context) != PPH_ERROR_OK){
 		pam_syslog(pamh, LOG_ERR, "PPH: Can't destroy context %d \n", error);
-		exit(1);
 	}
 	//lastly, return the right PAM value
 	if (error == PPH_ERROR_OK) {
@@ -227,7 +251,7 @@ void get_secret(pph_context *context){
 	fclose(secretfile);
 }
 
-void get_share(pph_context *context) {
+void get_share_context(pph_context *context) {
 	FILE *sharefile;
 	sharefile = fopen("/home/lolaly/PolyPasswordHasher-PAM/ramdisk/share", "r");
 	if (sharefile == NULL) {
@@ -242,6 +266,74 @@ void get_share(pph_context *context) {
 	fclose(sharefile);
 }
 
-	
+void store_account(const char* username, const char* password){
+	FILE *accountfile;
+	accountfile = fopen("/home/lolaly/PolyPasswordHasher-PAM/ramdisk/account", "a");
+	if (accountfile == NULL) {
+ 		return;
+	}
+	printf("saving account data!\n");
+	fprintf(accountfile, "%s\n", username);
+	fprintf(accountfile, "%s\n", password);
+	fclose(accountfile);
+}
+
+
+void store_secret(pph_context *context){
+	FILE* secretfile;
+        secretfile = fopen("/home/lolaly/PolyPasswordHasher-PAM/ramdisk/secret", "w+");
+        if (secretfile == NULL){
+          printf("can't save secret!" );
+        }else {
+          fwrite(context->secret, DIGEST_LENGTH, 1, secretfile);
+	}
+	fclose(secretfile);
+}
+void store_share_context(pph_context *context){
+	FILE* sharefile;
+	sharefile = fopen("/home/lolaly/PolyPasswordHasher-PAM/ramdisk/share", "w+");
+        if (sharefile == NULL){
+          	printf("can't save secret!" );
+        }else {
+		fwrite(context->share_context, sizeof(gfshare_ctx), 1, sharefile);
+		fwrite(context->share_context->sharenrs, context->share_context->sharecount, 1, sharefile);
+		fwrite(context->share_context->buffer, context->share_context->buffersize, 1, sharefile);
+	}
+	fclose(sharefile);
+
+}
+
+void unlock_context(pph_context *context){
+	FILE *accountfile;
+	accountfile = fopen("/home/lolaly/PolyPasswordHasher-PAM/ramdisk/account", "r");
+	if (accountfile != NULL) {
+		char pro_username[MAX_USERNAME_LENGTH];
+		char pro_password[MAX_PASSWORD_LENGTH];
+		int index = 0;
+		int size = context->threshold;
+		const uint8 **usernames = malloc(sizeof(*usernames)*size);
+  		const uint8  **passwords = malloc(sizeof(*passwords)*size);
+  		unsigned int *username_lengths = malloc(sizeof(*username_lengths)*size);
+		unsigned int *password_lengths = malloc(sizeof(*password_lengths)*size);
+		int unlockret;	
+		while (fscanf(accountfile, "%s\n%s\n", pro_username, pro_password)!= EOF){
+  			usernames[index] = strdup(pro_username); 
+ 			passwords[index] = strdup(pro_password);
+			username_lengths[index] = strlen(pro_username);
+			password_lengths[index] = strlen(pro_password);
+			index++;
+		}
+		unlockret = pph_unlock_password_data(context, index+1, usernames, username_lengths, passwords, password_lengths);
+		if (unlockret == PPH_ERROR_OK) {
+			store_secret(context);
+			store_share_context(context);
+			fclose(accountfile);
+			remove(accountfile);
+		} else {
+			fclose(accountfile);
+		}
+	}
+}
+
 
 
