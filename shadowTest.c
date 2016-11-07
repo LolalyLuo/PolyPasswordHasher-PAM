1222f619bae764a4e04ac972f5c4b3578af7d380
Lolaly <sl5419@nyu.edu>
2016-11-07 12:29:59 -0500

    functions are all finished, but still testing code

--- shadowTest.c
+++ shadowTest.c
@@ -14,7 +14,7 @@ int main() {
 	pph_context *context;
   	printf("initial context\n");
 
-	context = pph_init_context(2,3);
+	context = pph_init_context(1,2);
 	if (context == NULL){
 		printf("can't initialize context\n");
 		exit(1);
@@ -28,7 +28,7 @@ int main() {
                        "345",strlen("345"), 3);
 	printf("%d\n", error);
 	error = pph_create_account(context, "shadowtheabc", strlen("shadowtheabc"),
-                       "567",strlen("567"), 2);
+                       "567",strlen("567"), 1);
 	printf("%d\n", error);
 	error = pph_create_account(context, "marry", strlen("marry"),
                        "567",strlen("567"), 1);
@@ -42,6 +42,7 @@ int main() {
 	}
 	pph_destroy_context(context);
 	printf("context is initialed and stored!\n");
+
 	//Now reload the context and reload all the accounts
 	pph_context* reload_context = pph_reload_context("/home/lolaly/PolyPasswordHasher-PAM/PPHdata");
 	if (reload_context == NULL){
@@ -49,6 +50,13 @@ int main() {
 	}
 	printf("PPH context re---loaded\n");
 	load_accounts_shadow(reload_context);
+	error = pph_check_login(reload_context, "shadowabc", strlen("shadowabc"),
+                       "123",strlen("123"));
+	if (error == 0) {
+		printf("YEAH!!!! LOGIN SUCCESSFULLY!!!\n");
+	}else{
+		printf("YOU FAILEDDDDD!!!error: %d \n", error);
+	}
 } 
 
 
@@ -72,18 +80,51 @@ void load_accounts_shadow(pph_context *context){
 	
 	while(fgets(buffer, sizeof buffer, shadow) != NULL){
 		//fgets(buffer, 4096, shadow);
-		printf("buffer: %s\n", buffer);
 		sscanf(buffer, "%128[^:]:%525[^:]:%s\n", name, password, others);
-		printf("name->%s, password->%s, other->%s\n\n\n", name, password, others);
+		//printf("name->%s, password->%s, other->%s\n\n\n", name, password, others);
 		token = strtok(password, s);
 		if (strcmp(token, "PPH") == 0){
-			printf("IT IS A PPH ACCOUT\n");
-				sharenum = atoi(strtok(NULL, s));
-				hexsalt = strtok(NULL, s);
-				hexsxorh = strtok(NULL, s);
-				hexicb = strtok(NULL, s);
-				printf("token is: %d  %s  %s  %s  ENDENDEND\n", sharenum, hexsalt, hexsxorh, hexicb);
-			
+			//printf("IT IS A PPH ACCOUT\n");
+			//add a new account into context to hold the PPH account 
+			pph_account_node *current_node = malloc(sizeof(pph_account_node));
+			pph_account_node *temp_node = context->account_data;
+			context->account_data = current_node;
+			current_node->next = temp_node;
+			//set up data for that account 
+			strcpy(current_node->account.username, name);
+			current_node->account.username_length = strlen(name);
+			//use a while loop to put data in entry
+			int count_entries = 0;
+			token = strtok(NULL, s);
+			while (token != NULL){
+				pph_entry *current_entry = malloc(sizeof(pph_entry));
+				pph_entry *temp_entry = current_node->account.entries;
+				current_node->account.entries = current_entry;
+				current_entry->next = temp_entry;
+				//store data in
+				//storing share_number 
+				current_entry->share_number = atoi(token);
+				//storing salt
+				token = strtok(NULL, s);
+				for (int i = 0; i < strlen(token); i++){
+					sscanf(token+(i*2), "%02x", &current_entry->salt[i]);
+				}
+				current_entry->salt_length = strlen(current_entry->salt);
+				//store shore_xor_hash
+				token = strtok(NULL, s);
+				for (int i = 0; i < strlen(token); i++){
+					sscanf(token+(i*2), "%02x", &current_entry->sharexorhash[i]);
+				}
+				//store isolated check bits
+				token = strtok(NULL, s);
+				for (int i = 0; i < strlen(token); i++){
+					sscanf(token+(i*2), "%02x", &current_entry->isolated_check_bits[i]);
+				}
+				//before go back to the loop, increase count and read another token 
+				token = strtok(NULL, s);
+				count_entries++;
+			}
+			current_node->account.number_of_entries = count_entries;
 		}
 	}
 	fclose(shadow);
@@ -148,11 +189,10 @@ void store_accounts_shadow(pph_context *context) {
 	}
 
 	while(search!=NULL){
-		printf("name:%s\n", search->account.username);
 		struct spwd *target = getspnam(search->account.username);
 		if (target != NULL){
 			int i;
-			char result[1000] = "$PPH$";
+			char result[1000] = "$PPH";
 			pph_entry *entry_node = search->account.entries;
 		
 			while(entry_node != NULL){
@@ -160,7 +200,6 @@ void store_accounts_shadow(pph_context *context) {
 				char hexsalt[2*MAX_SALT_LENGTH + 1] = "\0";
 				char hexsxorh[2*DIGEST_LENGTH + 1] = "\0";
 				char hexicb[2*DIGEST_LENGTH + 1] = "\0";
-				printf("in second loop\n");
 				for (i = 0; i < MAX_SALT_LENGTH; i++) {
 					sprintf(buffer, "%0x", entry_node->salt[i]);
 					strcat(hexsalt, buffer);
@@ -178,7 +217,7 @@ void store_accounts_shadow(pph_context *context) {
 					strcat(hexicb, buffer);
 				}
 				hexicb[2 * icb_length+1] = '\0';
-				sprintf(buffer,"%d$%s$%s$%s$", entry_node->share_number,
+				sprintf(buffer,"$%d$%s$%s$%s", entry_node->share_number,
 					hexsalt, hexsxorh, hexicb);
 				strcat(result, buffer);
 				entry_node = entry_node->next;
@@ -186,7 +225,6 @@ void store_accounts_shadow(pph_context *context) {
 			
 			target->sp_pwdp = result;
 			retval = putspent(target, temp);
-			printf("RETVAL is:%d\n", retval);
 		}
 		search = search->next;
 	}
